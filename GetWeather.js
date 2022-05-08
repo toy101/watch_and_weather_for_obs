3f011e589f54ee7201e7f94e80b6456d9c2507b0
toy101 <toy101dev@gmail.com>
2022-05-09 00:17:51 +0900

    iconの設定

--- GetWeather.js
+++ GetWeather.js
@@ -35,6 +35,12 @@ let localDatas = [
         lon: 136.65969926711716,
     },
     {
+        cityName: "Matsumoto",
+        displayName: "松本",
+        lat: 36.23119908770519,
+        lon: 137.95602063773055,
+    },
+    {
         cityName: "Nagoya",
         displayName: "名古屋",
         lat: 35.257094766996595,
@@ -57,6 +63,12 @@ let localDatas = [
         displayName: "福岡",
         lat: 33.56735024709979,
         lon: 130.36474036426256,
+    },
+    {
+        cityName: "Naha",
+        displayName: "那覇",
+        lat: 26.211631088233275,
+        lon: 127.68229821934504,
     },
 ];
 
@@ -87,7 +99,27 @@ localDatas.forEach((city, idx) => {
         })
         .then((data) => {
             // console.log(data);
-            responsedWeather = data.daily;
+            var responsedWeather = data.daily;
+            var weatherCode = responsedWeather.weathercode[1];
+            switch (Math.floor(weatherCode / 10)) {
+                case 4:
+                    weatherCode = "4x";
+                    break;
+                case 5:
+                    weatherCode = "5x";
+                    break;
+                case 7:
+                    weatherCode = "7x";
+                    break;
+                case 8:
+                    if (![80, 81, 82].includes(weatherCode)) {
+                        weatherCode = "8x";
+                    }
+                    break;
+                case 9:
+                    weatherCode = "9x";
+                    break;
+            }
             weatherList.innerHTML += `<div class="weather" id=${city.cityName}>
                                         <div class="text-data">
                                             <div class="local-label">${
@@ -112,10 +144,8 @@ localDatas.forEach((city, idx) => {
                                             class="weather-icon"
                                             id=${"img-" + city.cityName}
                                             src=${
-                                                "./icons/0" +
-                                                (responsedWeather
-                                                    .weathercode[1] %
-                                                    10) +
+                                                "./icons/" +
+                                                weatherCode +
                                                 ".png"
                                             }
                                             alt="Weather icon"
